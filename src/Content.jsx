f5eb07ed4f2f2d819bb2fc2175f95c914a331f4c
jordanoboyle <jmcmcode@gmail.com>
2024-07-11 21:42:17 -0600

    working modal

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
 import { PostsIndex } from "./PostIndex"
+import { Modal } from "./Modal"
 
 
 export function Content() {
   const [posts, setPosts] = useState([])
+  const [isPostShowVisible, setIsPostShowVisible] = useState(false);
+  const [currentPost, setCurrentPost] = useState({})
 
 
   const handleIndexPosts = () => {
@@ -12,15 +15,31 @@ export function Content() {
     axios.get("http://localhost:3000/posts.json").then(response => {
       console.log(response.data);
       setPosts(response.data);
-      
     })
+    .catch(error => {
+      console.error("There was an error fetching the posts!", error);
+    });
   }
+
+  const handleCloseModal = () => {
+    console.log("closing modal");
+    setIsPostShowVisible(false)
+  }
+  const handleShowSinglePost = (bike) => {
+    console.log("showing single post through modal");
+    setIsPostShowVisible(true);
+  }
+
   useEffect(handleIndexPosts, [])
 
   return (
     <main>
-      <h1>Welcome to React!</h1>
-      <PostsIndex posts={posts}/>
+      <h1>The Platonic Platypus</h1>
+      <PostsIndex posts={posts} onShowPost={handleShowSinglePost}/>
+      <Modal show={isPostShowVisible} onClose={handleCloseModal}>
+      Think about this like html content
+      
+      </Modal>
     </main>
   )
 }
