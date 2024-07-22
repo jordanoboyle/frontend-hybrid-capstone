0e65ba4b2f8cb838d0997c7b037c17bbdd43bb1d
jordanoboyle <jmcmcode@gmail.com>
2024-07-21 22:10:54 -0600

    Homepage layout change, centered, but basic. Needs work.

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -4,22 +4,60 @@ import { Button } from "react-bootstrap"
 export function HomePage() {
   const navigate = useNavigate()
   return(
-    <div>
-      <h1>Test from the Home Page</h1>
-      <h1>Welcome the Platonic Platypus Lounge</h1>
-      <div>
-        <h2>Visit the blog. Become a contributor or read from the thoughts of your fellow contributors.</h2>
-        <Button variant="secondary" onClick={() => navigate("/postIndex")}>Blog</Button>
-      </div>
-      <div>
-        <h2>Ask questions and find answers to other inquiries in the FAQ Section</h2>
-        <button onClick={() => navigate("/indexFaqs")}>FAQs Section</button>
-      </div>
-      <div>
-        <h2>See what our esteemed contributors through of games and systems, both new and retro.</h2>
-        <button onClick={() => navigate("/indexReviews")}>Reviews</button>
+    <div style={outerContainerStyle}>
+      <div style={innerContainerStyle}>
+        <h1>Welcome to the Platonic Platypus Lounge</h1>
+        <div>
+          <h2>Visit the blog. Become a contributor or read from the thoughts of your fellow contributors.</h2>
+          <Button variant="secondary" onClick={() => navigate("/postIndex")} style={buttonStyle}>Blog</Button>
+        </div>
+        <div>
+          <h2>Ask questions and find answers to other inquiries in the FAQ Section</h2>
+          
+          <Button variant="primary" size="lg" onClick={() => navigate("/indexFaqs")} style={buttonStyle}>
+            FAQs
+          </Button>
+        </div>
+        <div>
+          <h2>See what our esteemed contributors think of games and systems, both new and retro.</h2>
+          <Button variant="primary" size="lg" onClick={() => navigate("/indexReviews")} style={buttonStyle} >
+           Reviews
+          </Button>
+        </div>
       </div>
     </div>
   )
 }
 
+
+
+const outerContainerStyle = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: 'center',
+  height: '100vh',
+  backgroundImage: 'url(/public/DefaultImages/BGImage.jpeg)', 
+  backgroundSize: 'cover',
+  backgroundPosition: 'center'
+}
+const innerContainerStyle = {
+  backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent background
+  color: 'white',
+  padding: '20px',
+  borderRadius: '10px',
+  textAlign: 'center',
+  maxWidth: '80%',
+  width: '80%',
+};
+
+const buttonStyle = {
+  backgroundColor: 'black',
+  color: 'white',
+  border: 'none',
+  padding: '2em 2em',
+  fontSize: '1em',
+  margin: '10px 0',
+  borderRadius: '5px',
+  width: '40%',
+  cursor: 'pointer',
+};
